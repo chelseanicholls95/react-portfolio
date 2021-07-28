9bbc4cc3a82868544c37f19b4c91e6b901a5b4f8
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-07-28 16:01:10 +0100

    added view state to render sections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { Component } from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavbarComponent from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <NavbarComponent />
-      <Header />
-      <AboutMe />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      view: "home",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <NavbarComponent />
+        <Header />
+        {this.state.view === "aboutMe" && <AboutMe />}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;

08c0e5707a8c8281fafa81c9a6058105ec6b0c09
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-07-28 16:00:35 +0100

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Coded by: Chelsea Nicholls. Graduating: August 2021. Available to hire
+      from: August 2021.
+    </footer>
+  );
+};
+
+export default Footer;

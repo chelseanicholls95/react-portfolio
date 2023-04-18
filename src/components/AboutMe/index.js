cb67541ffb9b0a0dcb68479d2196859c6fa935eb
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2023-04-18 20:17:35 +0100

    edited about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -11,25 +11,26 @@ const AboutMe = () => {
         <h2>About Me</h2>
         <div>
           <p>
-            Hi! I’m Chelsea Nicholls and I am currently a student at the
-            University of Birmingham Full-Stack Web Development Coding Bootcamp.
-            I will be graduating in August 2021 with an excellent understanding
-            of both front-end and back-end web development. I currently have a
-            proficient knowledge in HTML and CSS and by the end of the course
-            will have an in-depth understanding of JavaScript and the MERN
-            stack.
+            Hi! I'm Chelsea Nicholls and I am an aspiring Full Stack Software
+            Engineer. I graduated from the University of Birmingham Full Stack
+            Web Development Coding Bootcamp in 2021 where I became proficient in
+            HTML, CSS, Javascript and the MERN stack. Since then I have had a
+            baby and I am now ready to enter the workforce as a Software
+            Engineer. I have been keeping my skills up to date by completing
+            online activities and creating small personal projects.
           </p>
           <p>
-            I graduated from University with a degree in Forensic Psychology and
-            had realised that it was not the field that I wanted to go into. I
-            had really enjoyed the data analysis and problem-solving side of the
-            degree and I decided I wanted a more technical career. Fast forward
-            to the start of the pandemic and I found an online course which
-            taught the basics of HTML, CSS and JavaScript. I immediately fell in
-            love and knew this was the career path for me. I love that web
-            development is a field that is always expanding and there is always
-            something new to learn. I plan to continue to expand my knowledge of
-            varying languages after I have completed the bootcamp.
+            Back in 2018, I graduated from University with a degree in Forensic
+            Psychology and had realised that it was not the field that I wanted
+            to go into. I had really enjoyed the data analysis and
+            problem-solving side of the degree and I decided I wanted a more
+            technical career. Fast forward to the start of the pandemic and I
+            found an online course which taught the basics of HTML, CSS and
+            JavaScript. I immediately fell in love and knew this was the career
+            path for me. I love that web development is a field that is always
+            expanding and there is always something new to learn. I plan to
+            continue to expand my knowledge of varying languages after I have
+            completed the bootcamp.
           </p>
           <p>
             I have a variety of other interests including cooking, walking and
